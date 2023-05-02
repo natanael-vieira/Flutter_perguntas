76c1c8b4fc6e17956378f93e879209de3804b401
Natanael Vieira <natanaelsales@live.com>
2023-05-02 06:18:01 -0300

    Create questionario.dart

---
+++ projeto_perguntas/lib/questionario.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './respostas.dart';
+
+
+class Questionario extends StatelessWidget {
+  Questionario({
+    required this.perguntas,
+    required this.perguntaSelecionada,
+    required this.responder,
+  });
+
+  final List<Map<String, Object>> perguntas;
+  final int perguntaSelecionada;
+  final void Function() responder;
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]['respostas'] as List<String>
+        : [];
+    return Column(
+      children: [
+        Questao(perguntas[perguntaSelecionada]['texto'] as String),
+        ...respostas.map((t) => Respostas(t, responder)).toList(),
+      ],
+    );
+  }
+}
