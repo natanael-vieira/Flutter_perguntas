34ab6fd5251cf0ea423b51f27d97690331d4d83f
Natanael Vieira <natanaelsales@live.com>
2023-05-02 05:23:06 -0300

    Update main.dart

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
 import './respostas.dart';
+import './resultado.dart';
 
 void main() => runApp(const PerguntaApp());
 
@@ -52,12 +53,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   ...respostas.map((t) => Respostas(t, _responder)).toList(),
                 ],
               )
-            : const Center(
-              child: Text('Parabéns!',
-              style: TextStyle(
-                fontSize: 28
-              ),),
-            ),
+            : const Resultado(), 
       ),
     );
   }
